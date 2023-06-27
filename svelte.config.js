50737b6570ab28404b44b057e6fd19dd0d16ef4e
saiksaif <116622210+saiksaif@users.noreply.github.com>
2023-06-27 19:48:29 +0500

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
-// import adapter from '@sveltejs/adapter-static';
-
-// export default {
-//     kit: {
-//         adapter: adapter({
-//             // default options are shown. On some platforms
-//             // these options are set automatically — see below
-//             pages: 'build',
-//             assets: 'build',
-//             fallback: undefined,
-//             precompress: false,
-//             strict: true
-//         })
-//     }
-// };
-// import { defineConfig } from '@sveltejs/kit';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
-  adapter: '@sveltejs/adapter-static',
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
 };
