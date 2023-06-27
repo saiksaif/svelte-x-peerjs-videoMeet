5f99e15122d0a378b81e7370f7dfc0906998214b
saiksaif <116622210+saiksaif@users.noreply.github.com>
2023-06-27 19:48:29 +0500

    revert

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-    target: 'static',
-  })],
-});
+  plugins: [svelte()],
+})
